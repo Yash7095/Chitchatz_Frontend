689e2a4edd9337582554b0f027d08fc3641f6f40
yash_gupta2 <yash.gupta@faspi.in>
2026-05-06 09:40:00 +0530

    updated code

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -6,6 +6,6 @@ import axios from "axios";
 // });
 
 export const axiosInstance = axios.create({
-  baseURL: "http://192.168.1.196:5001/api",
+  baseURL: "http://192.168.1.16:5001/api",
   withCredentials: true,
 });
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL =
   // import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
-  import.meta.env.MODE === "development" ? "http://192.168.1.196:5001" : "/";
+  import.meta.env.MODE === "development" ? "http://192.168.1.16:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
